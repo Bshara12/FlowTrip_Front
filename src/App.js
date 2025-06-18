ed456b4f24502c4bdc3cf247ce2e4593a86b2749
Bshara12 <bshara.h2003@gmail.com>
2025-06-18 19:22:13 +0300

    remember me&forgetpass

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Auth from "./Auth/Auth";
 import DashBourd from "./Admin/DashBourd";
+import OwnerRigester from "./Auth/OwnerRigester";
 
 function App() {
   return (
     <Routes>
       <Route path="register" element={<Auth/>}/>
+      <Route path="ownerrigester" element={<OwnerRigester/>}/>
 
       {/* Admin */}
       <Route>

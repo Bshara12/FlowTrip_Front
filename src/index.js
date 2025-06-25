bd87847c873e9bc92bca325d6b1e0815ac300f82
FerasDeveloper <ferashatem38@gmail.com>
2025-06-25 12:23:27 +0300

    Show Owners and Filter

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import "./css/all.min.css";
+import "./Assets/all.min.css"
 import { BrowserRouter } from "react-router-dom";
 
 import { colors } from "./Component/Color.js";

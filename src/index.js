83ca5101a48e425020788f40d30ec0de103ae372
FerasDeveloper <ferashatem38@gmail.com>
2025-06-25 12:41:25 +0300

    correction

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import "./css/all.min.css";
-import "./Assets/all.min.css"
 import { BrowserRouter } from "react-router-dom";
 
 import { colors } from "./Component/Color.js";
